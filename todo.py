daa19f2205191650abe817babed8cbdddb5b2dfc
Adrel410 <ogundipe_adeola@columbusstate.edu>
2022-09-24 15:04:03 +0000

    added a form to the homepage

--- todo.py
+++ todo.py
@@ -3,9 +3,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template["home.html"]
+
+@app.route('/addTodo', method=['POST'])
+def addTodo():
+    #access body information
+
 
 if __name__ == "__main__":
     app.run(debug=True)
